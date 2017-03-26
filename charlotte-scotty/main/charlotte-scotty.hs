fadbfbafc8726d2e361250e6df797d78f681866a
Charles O'Farrell <charleso@charleso.org>
2017-03-26 11:26:33 +1100

    Scotty

---
+++ charlotte-scotty/main/charlotte-scotty.hs
@@ -0,0 +1,71 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import           Charlotte.App
+
+import           Data.Monoid ((<>))
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy.Encoding as TL
+
+import qualified Network.HTTP.Types as HTTP
+
+import           Web.Scotty (ActionM, ScottyM, get, post, redirect, param, status, html, scotty)
+import           Web.Scotty.Cookie (getCookie, setCookie, makeSimpleCookie)
+
+main :: IO ()
+main =
+  scotty 8081 routes
+
+--------
+
+routes :: ScottyM ()
+routes = do
+  get "/" $
+    secure homeGet
+  get "/login" $
+    loginGet
+  post "/login" $
+    loginPost
+  get "/profile/:user" $ do
+    name <- User <$> param "user"
+    secure (profileGet name)
+
+homeGet :: Session -> ActionM ()
+homeGet session =
+  redirect . TL.fromStrict $
+    "/profile/" <> (renderUser . sessionUser) session
+
+loginGet :: ActionM ()
+loginGet =
+  html' $
+    loginView Nothing
+
+loginPost :: ActionM ()
+loginPost = do
+  user <- param "username"
+  setCookie $
+    makeSimpleCookie "session" user
+  redirect . TL.fromStrict $
+    "/profile/" <> user
+
+profileGet :: User -> Session -> ActionM ()
+profileGet user session =
+  if sessionUser session /= user then do
+    status HTTP.status403
+    html' $ htmlTemplate "Unauthorized"
+  else do
+    status HTTP.status200
+    html' $ htmlTemplate "Authorized"
+
+secure :: (Session -> ActionM ()) -> ActionM ()
+secure f = do
+  c <- getCookie "session"
+  case c of
+    Nothing ->
+      redirect "/login"
+    Just s ->
+      f (Session (User s))
+
+html' :: Html -> ActionM ()
+html' =
+  -- TODO Should be text, not bytestring, if nothing else
+  html . TL.decodeUtf8
